a66661980de87ef95571d7fd32d9a0cd53186aa2
Nick Tate <ntate6630@yahoo.co.uk>
2016-12-15 12:41:26 +0000

    Add update menus

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
-angular.module('cookingApp', ['ngRoute', 'RouteControllers']);
- 
-angular.module('cookingApp').config(function($routeProvider) {
- 
-    $routeProvider.when('/register', {
+angular.module('cookingapp', ['ngRoute', 'angular-storage', 'RouteControllers', 'UserService']);
+
+angular.module('cookingapp').config(function($routeProvider) {
+
+	$routeProvider.when('/accounts/register', {
 		templateUrl: 'templates/register.html',
 		controller: 'RegisterController'
-	/*})
-	.when('/login', {
-		templateURL: 'templates/login.html',
+	});/*
+	.when('/accounts/login', {
+		templateUrl: 'templates/login.html',
 		controller: 'LoginController'
 	})
-	.when('/logout', {
-		templateURL: 'templates/logout.html',
-		controller: 'LogoutController',
+	.when('/accounts/logout', {
+		templateUrl: 'templates/logout.html',
+		controller: 'LogoutController'
 	})
 	.when('/recipes', {
-		templateURL: 'templates/recipes.html',
+		templateUrl: 'templates/recipes.html',
 		controller: 'RecipesController',
 	})
 	.when('/howTo', {
-		templateURL: 'templates/howto.html',
+		templateUrl: 'templates/howto.html',
 		controller: 'HowToController',
 	})
 	.when)'/video', {
-		templateURL: 'templates/video.html',
+		templateUrl: 'templates/video.html',
 		controller: 'VideoController',
 	})
 	.when('/shop', {
-		templateURL: 'templates/shop.html',
+		templateUrl: 'templates/shop.html',
 		controller: 'ShopController',
-	*/});
+	});*/
 });
--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,58 @@
 angular.module('RouteControllers', [])
-    .controller('RegisterController', function($scope) {
- 
+    .controller('RegisterController', function($scope, $location, UserAPIService, store) {
+
         $scope.registrationUser = {};
- 
+        var url = "https://morning-castle-91468.herokuapp.com/";
+
+        $scope.login = function() {
+            UserAPIService.callAPI(url + "accounts/api-token-auth/", $scope.data).then(function(results) {
+                $scope.token = results.data.token;
+                store.set('username', $scope.registrationUser.username);
+                store.set('authToken', $scope.token);
+                $location.path("/todo");
+            }).catch(function(err) {
+                console.log(err.data);
+            });
+        }
+
         $scope.submitForm = function() {
             if ($scope.registrationForm.$valid) {
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
+
+                UserAPIService.callAPI(url + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    if ($scope.data.username == $scope.registrationUser.username 
+                        && $scope.data.password == $scope.registrationUser.password) {
+                        
+                        $scope.login();
+                    }
+                }).catch(function(err) {
+                    console.log(err)
+                });
             }
- 
-            console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
         }
+    })
+    .controller('LoginController', function($scope, $location, UserService, store) {
+        var url = "https://morning-castle-91468.herokuapp.com/";
+
+        $scope.submitForm = function() {
+            if ($scope.loginForm.$isValid) {
+                $scope.loginUser.username = $scope.user.username;
+                $scope.loginUser.password = $scope.user.password;
+
+                UserAPIService.callAPI(url + "accounts/api-token-auth", $scope.data).then(function(results) {
+                    $scope.token = results.data.token;
+                    store.set('username', $scope.loginUser.username);
+                    store.set('authToken', $scope.token);
+                    $location.path("/todo");
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            }
+        };
+    })
+	.controller('LogoutController', function(store) {
+        store.remove('username');
+        store.remove('authToken');
     });
